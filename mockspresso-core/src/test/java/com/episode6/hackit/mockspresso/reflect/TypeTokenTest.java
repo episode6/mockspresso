e6e8dc75e5c552a2f296eea53f9ca6edfdef6073
Geoff Hackett <ghackett@gmail.com>
2017-01-19 14:00:16 -0500

    add TypeToken test to ensure generics are accurately represented

--- mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/reflect/TypeTokenTest.java
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/reflect/TypeTokenTest.java
@@ -4,6 +4,12 @@ import com.episode6.hackit.mockspresso.DefaultTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 
 /**
@@ -42,5 +48,27 @@ public class TypeTokenTest {
         .isEqualTo(Integer.class);
     assertThat((Class)intToken.getRawType())
         .isEqualTo(Integer.class);
+  }
+
+  @Test
+  public void testSimpleGenerics() {
+    TypeToken<List<String>> stringListToken = new TypeToken<List<String>>() {};
+    TypeToken<HashMap<String, Integer>> hashmapTypeToken = new TypeToken<HashMap<String, Integer>>() {};
+
+    assertThat(stringListToken.getType()).isInstanceOf(ParameterizedType.class);
+    Type[] stringListTypeArgs = ((ParameterizedType)stringListToken.getType()).getActualTypeArguments();
+    assertThat(stringListTypeArgs)
+        .hasSize(1)
+        .containsExactly(String.class);
+    assertThat((Class)stringListToken.getRawType())
+        .isEqualTo(List.class);
+
+    assertThat(hashmapTypeToken.getType()).isInstanceOf(ParameterizedType.class);
+    Type[] hashmapTypeArgs = ((ParameterizedType)hashmapTypeToken.getType()).getActualTypeArguments();
+    assertThat(hashmapTypeArgs)
+        .hasSize(2)
+        .containsExactly(String.class, Integer.class);
+    assertThat((Class)hashmapTypeToken.getRawType())
+        .isEqualTo(HashMap.class);
   }
 }
