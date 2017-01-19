21dc4819bf6574188f9be08a0f9f79f9800dd8d2
Geoff Hackett <ghackett@gmail.com>
2017-01-19 13:11:27 -0500

    make a test package

---
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/MockspressoTest.java
@@ -0,0 +1,20 @@
+package com.episode6.hackit.mockspresso;
+
+import com.episode6.hackit.chop.Chop;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Main mockspresso test
+ */
+@RunWith(DefaultTestRunner.class)
+public class MockspressoTest {
+
+  /**
+   * placeholder test
+   */
+  @Test
+  public void placeholderTest() {
+    Chop.i("TEST SUCCESS");
+  }
+}
