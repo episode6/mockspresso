a61b57243565ee203da8a5428c8f2e6c438767d4
Geoff Hackett <ghackett@gmail.com>
2017-03-01 17:23:08 -0500

    add DependencyKey class and test its equality

---
+++ mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/api/DependencyKey.java
@@ -0,0 +1,44 @@
+package com.episode6.hackit.mockspresso.api;
+
+import com.episode6.hackit.mockspresso.reflect.ReflectUtil;
+import com.episode6.hackit.mockspresso.reflect.TypeToken;
+
+import javax.annotation.Nullable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+/**
+ * Key used to identify objects and dependencies
+ */
+public final class DependencyKey<V> {
+
+  public static DependencyKey<?> fromField(Field field) {
+    return new DependencyKey<>(TypeToken.of(field), ReflectUtil.findQualifierAnnotation(field));
+  }
+
+  public final TypeToken<V> typeToken;
+  public final @Nullable Annotation identityAnnotation;
+
+  public DependencyKey(TypeToken<V> typeToken, @Nullable Annotation identityAnnotation) {
+    this.typeToken = typeToken;
+    this.identityAnnotation = identityAnnotation;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    DependencyKey<?> that = (DependencyKey<?>) o;
+
+    if (!typeToken.equals(that.typeToken)) return false;
+    return identityAnnotation != null ? identityAnnotation.equals(that.identityAnnotation) : that.identityAnnotation == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = typeToken.hashCode();
+    result = 31 * result + (identityAnnotation != null ? identityAnnotation.hashCode() : 0);
+    return result;
+  }
+}
---
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/api/DependencyKeyTest.java
@@ -0,0 +1,62 @@
+package com.episode6.hackit.mockspresso.api;
+
+import com.episode6.hackit.mockspresso.DefaultTestRunner;
+import com.episode6.hackit.mockspresso.annotation.RealObject;
+import com.episode6.hackit.mockspresso.annotation.TestQualifierAnnotation;
+import com.episode6.hackit.mockspresso.exception.MultipleQualifierAnnotationException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Named;
+import javax.inject.Provider;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * Tests {@link DependencyKey}
+ */
+@RunWith(DefaultTestRunner.class)
+public class DependencyKeyTest {
+
+  @Named("test1") Integer testProp1;
+  @Named HashMap<String, Provider<Integer>> testProp2;
+  @Named @RealObject HashMap<String, Provider<Integer>> testProp3;
+  @RealObject @Named HashMap<String, Provider<String>> testProp4;
+  @RealObject @Named("test1") Integer testProp5;
+
+  @Named @RealObject @TestQualifierAnnotation String badTestProp;
+
+
+  @Test
+  public void testKeyEquality() {
+    DependencyKey key1 = DependencyKey.fromField(prop("testProp1"));
+    DependencyKey key2 = DependencyKey.fromField(prop("testProp2"));
+    DependencyKey key3 = DependencyKey.fromField(prop("testProp3"));
+    DependencyKey key4 = DependencyKey.fromField(prop("testProp4"));
+    DependencyKey key5 = DependencyKey.fromField(prop("testProp5"));
+
+    assertThat(key1)
+        .isEqualTo(key5)
+        .isNotEqualTo(key2)
+        .isNotEqualTo(key4);
+    assertThat(key2)
+        .isEqualTo(key3)
+        .isNotEqualTo(key4)
+        .isNotEqualTo(key5);
+  }
+
+  @Test(expected = MultipleQualifierAnnotationException.class)
+  public void testMultipleQualifiersFailure() {
+    DependencyKey.fromField(prop("badTestProp"));
+  }
+
+  private static Field prop(String name) {
+    try {
+      return DependencyKeyTest.class.getDeclaredField(name);
+    } catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
