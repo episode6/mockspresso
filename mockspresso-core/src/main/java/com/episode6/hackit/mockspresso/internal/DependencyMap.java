37d1d890e563f91983ff2022dcaf83796c1fbbd6
Geoff Hackett <ghackett@gmail.com>
2017-03-02 11:01:27 -0500

    Add a place to store explicitly mapped dependencies.

---
+++ mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/internal/DependencyMap.java
@@ -0,0 +1,27 @@
+package com.episode6.hackit.mockspresso.internal;
+
+import com.episode6.hackit.mockspresso.api.DependencyKey;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+
+/**
+ * Simple class to store mapped dependencies and type-check their fetch and retrieval.
+ */
+public class DependencyMap {
+  private final HashMap<DependencyKey, Object> mDependencies;
+
+  public DependencyMap() {
+    mDependencies = new HashMap<>();
+  }
+
+  @SuppressWarnings("unchecked")
+  public @Nullable <T, V extends T> T put(DependencyKey<T> key, V value) {
+    return (T) mDependencies.put(key, value);
+  }
+
+  @SuppressWarnings("unchecked")
+  public @Nullable <T> T get(DependencyKey<T> key) {
+    return (T) mDependencies.get(key);
+  }
+}
---
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/internal/DependencyMapTest.java
@@ -0,0 +1,49 @@
+package com.episode6.hackit.mockspresso.internal;
+
+import com.episode6.hackit.mockspresso.api.DependencyKey;
+import com.episode6.hackit.mockspresso.reflect.TypeToken;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * Tests {@link DependencyMap}
+ */
+public class DependencyMapTest {
+
+  interface TestInterface {}
+
+  class TestClass implements TestInterface {}
+
+  private DependencyMap mDependencyMap;
+
+  @Before
+  public void setup() {
+    mDependencyMap = new DependencyMap();
+  }
+
+  @Test
+  public void testSimplePutAndGet() {
+    TestClass value = new TestClass();
+    DependencyKey<TestClass> key = new DependencyKey<>(TypeToken.of(TestClass.class), null);
+
+    mDependencyMap.put(key, value);
+    TestClass output = mDependencyMap.get(key);
+
+    assertThat(value).isEqualTo(output);
+  }
+
+  @Test
+  public void testPutImplGetInterface() {
+    TestClass value = new TestClass();
+    DependencyKey<TestInterface> key = new DependencyKey<>(TypeToken.of(TestInterface.class), null);
+
+    mDependencyMap.put(key, value);
+    TestInterface output = mDependencyMap.get(key);
+
+    assertThat(output)
+        .isInstanceOf(TestClass.class)
+        .isEqualTo(value);
+  }
+}
