203ab10c9c14ccdfb7d8bda741a5d54a0fa19030
Geoff Hackett <ghackett@gmail.com>
2017-03-06 13:12:39 -0500

    Add some classes to deal with MethodRules. Think I will convert the MockspressoRule to be a methodRule, and these classes will help us and users deal with chaning MethodRules

---
+++ mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/junit/MethodRules.java
@@ -0,0 +1,34 @@
+package com.episode6.hackit.mockspresso.junit;
+
+import org.junit.rules.MethodRule;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+
+/**
+ * Utility methods for Method Rules
+ */
+public class MethodRules {
+
+  /**
+   * Wrap a {@link TestRule} in a {@link MethodRule} for compatibility.
+   */
+  public static MethodRule wrapTestRule(TestRule testRule) {
+    return new MethodRuleWrapper(testRule);
+  }
+
+  private static class MethodRuleWrapper implements MethodRule {
+    private final TestRule mTestRule;
+
+    private MethodRuleWrapper(TestRule testRule) {
+      mTestRule = testRule;
+    }
+
+    @Override
+    public Statement apply(Statement base, FrameworkMethod method, Object target) {
+      final Description description = Description.createTestDescription(target.getClass(), method.getName());
+      return mTestRule.apply(base, description);
+    }
+  }
+}
