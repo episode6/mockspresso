df0081ace9b922c282c2e16a011912d36ada8281
Geoff Hackett <ghackett@gmail.com>
2017-03-08 19:50:43 -0500

    Don’t really need this generic ObjectMaker interface

--- mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/api/SpecialObjectMaker.java
+++ mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/api/SpecialObjectMaker.java
@@ -3,9 +3,11 @@ package com.episode6.hackit.mockspresso.api;
 import com.episode6.hackit.mockspresso.reflect.DependencyKey;
 
 /**
- * An extension of {@link ObjectMaker} intended for special objects
- * that shouldn't be simply mocked by default.
+ * A class that creates "special" objects, i.e. objects that, by default, should not
+ * be simply mocked. An example of this is {@link javax.inject.Provider}, where we'd
+ * rather return a real Provider that provides a mock (or a mapped dependency).
  */
-public interface SpecialObjectMaker extends ObjectMaker {
+public interface SpecialObjectMaker {
   boolean canMakeObject(DependencyKey<?> key);
+  <T> T makeObject(DependencyProvider dependencyProvider, DependencyKey<T> key);
 }
