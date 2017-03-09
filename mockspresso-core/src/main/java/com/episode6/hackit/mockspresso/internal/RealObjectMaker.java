5b232a40afc0fdf59827b756367793d054469916
Geoff Hackett <ghackett@gmail.com>
2017-03-09 16:23:20 -0500

    throw exception when no constructor found.

--- mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/internal/RealObjectMaker.java
+++ mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/internal/RealObjectMaker.java
@@ -2,9 +2,11 @@ package com.episode6.hackit.mockspresso.internal;
 
 import com.episode6.hackit.mockspresso.api.DependencyProvider;
 import com.episode6.hackit.mockspresso.api.InjectionConfig;
+import com.episode6.hackit.mockspresso.exception.NoValidConstructorException;
 import com.episode6.hackit.mockspresso.reflect.TypeToken;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
 import java.util.List;
 
 /**
@@ -20,6 +22,10 @@ public class RealObjectMaker  {
   }
 
   public <T> T createObject(DependencyProvider dependencyProvider, TypeToken<T> typeToken) {
+    Constructor<T> constructor = mConstructorSelector.chooseConstructor(typeToken);
+    if (constructor == null) {
+      throw new NoValidConstructorException(typeToken);
+    }
     return null;
   }
 }
