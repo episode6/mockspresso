bc303c568aa6d8452dac319e9c3c9bad0883345a
Geoff Hackett <ghackett@gmail.com>
2017-03-10 19:36:42 -0500

    fill out RealObjectMaker and add some basic tests for it

--- mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/internal/RealObjectMaker.java
+++ mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/internal/RealObjectMaker.java
@@ -3,10 +3,16 @@ package com.episode6.hackit.mockspresso.internal;
 import com.episode6.hackit.mockspresso.api.DependencyProvider;
 import com.episode6.hackit.mockspresso.api.InjectionConfig;
 import com.episode6.hackit.mockspresso.exception.NoValidConstructorException;
+import com.episode6.hackit.mockspresso.reflect.DependencyKey;
+import com.episode6.hackit.mockspresso.reflect.ReflectUtil;
 import com.episode6.hackit.mockspresso.reflect.TypeToken;
 
+import javax.annotation.Nullable;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
 import java.util.List;
 
 /**
@@ -24,10 +30,56 @@ public class RealObjectMaker  {
   }
 
   public <T> T createObject(DependencyProvider dependencyProvider, TypeToken<T> typeToken) {
+    try {
+      return createObjectInternal(dependencyProvider, typeToken);
+    } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private <T> T createObjectInternal(DependencyProvider dependencyProvider, TypeToken<T> typeToken) throws IllegalAccessException, InvocationTargetException, InstantiationException {
     Constructor<T> constructor = mConstructorSelector.chooseConstructor(typeToken);
     if (constructor == null) {
       throw new NoValidConstructorException(typeToken);
     }
-    return null;
+
+    int paramCount = constructor.getParameterCount();
+    Type[] paramTypes = constructor.getGenericParameterTypes();
+    Annotation[][] paramAnnotations = constructor.getParameterAnnotations();
+    Object[] paramValues = new Object[paramCount];
+
+    for (int i = 0; i<paramCount; i++) {
+      TypeToken<?> paramToken = TypeToken.of(paramTypes[i]);
+      @Nullable Annotation qualifierAnnotation = ReflectUtil.findQualifierAnnotation(
+          paramAnnotations[i],
+          String.format("Constructor (%s), Param (%s)", typeToken, paramToken));
+      DependencyKey<?> paramKey = new DependencyKey<>(paramToken, qualifierAnnotation);
+      paramValues[i] = dependencyProvider.get(paramKey);
+    }
+
+    if (!constructor.isAccessible()) {
+      constructor.setAccessible(true);
+    }
+
+    T instance = constructor.newInstance(paramValues);
+    postProcessNewObject(dependencyProvider, instance);
+    return instance;
+  }
+
+  private void postProcessNewObject(DependencyProvider dependencyProvider, Object instance) throws IllegalAccessException {
+    if (mInjectFieldAnnotations.isEmpty()) {
+      return;
+    }
+
+    for (Field field : ReflectUtil.getAllDeclaredFields(instance.getClass())) {
+      if (ReflectUtil.isAnyAnnotationPresent(field, mInjectFieldAnnotations)) {
+        DependencyKey<?> paramKey = DependencyKey.fromField(field);
+        Object paramValue = dependencyProvider.get(paramKey);
+        if (!field.isAccessible()) {
+          field.setAccessible(true);
+        }
+        field.set(instance, paramValue);
+      }
+    }
   }
 }
---
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/internal/RealObjectMakerTest.java
@@ -0,0 +1,153 @@
+package com.episode6.hackit.mockspresso.internal;
+
+import com.episode6.hackit.mockspresso.DefaultTestRunner;
+import com.episode6.hackit.mockspresso.api.DependencyProvider;
+import com.episode6.hackit.mockspresso.api.InjectionConfig;
+import com.episode6.hackit.mockspresso.reflect.DependencyKey;
+import com.episode6.hackit.mockspresso.reflect.NamedAnnotationLiteral;
+import com.episode6.hackit.mockspresso.reflect.TypeToken;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests {@link RealObjectMaker}
+ */
+@RunWith(DefaultTestRunner.class)
+public class RealObjectMakerTest {
+
+  static final DependencyKey<Runnable> runnableKey = new DependencyKey<>(TypeToken.of(Runnable.class), null);
+  static final DependencyKey<Provider<Runnable>> runnableProviderKey = new DependencyKey<>(
+      new TypeToken<Provider<Runnable>>() {},
+      new NamedAnnotationLiteral("testprovider"));
+
+  @Mock InjectionConfig.ConstructorSelector mConstructorSelector;
+  @Mock DependencyProvider mDependencyProvider;
+
+  @Mock Runnable mRunnableMock;
+  @Mock Provider<Runnable> mRunnableProviderMock;
+
+  private RealObjectMaker mRealObjectMaker;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    // testing with first constructor available
+    when(mConstructorSelector.chooseConstructor(any(TypeToken.class)))
+        .thenAnswer(new Answer<Constructor>() {
+          @Override
+          public Constructor answer(InvocationOnMock invocation) throws Throwable {
+            TypeToken typeToken = invocation.getArgument(0);
+            return typeToken.getRawType().getDeclaredConstructors()[0];
+          }
+        });
+  }
+
+  @Test
+  public void testWithConstructorOnly() {
+    prep();
+
+    when(mDependencyProvider.get(runnableKey)).thenReturn(mRunnableMock);
+    when(mDependencyProvider.get(runnableProviderKey)).thenReturn(mRunnableProviderMock);
+
+    TestClassWithConstructorOnly testObject = mRealObjectMaker.createObject(
+        mDependencyProvider,
+        TypeToken.of(TestClassWithConstructorOnly.class));
+
+    verify(mDependencyProvider).get(runnableKey);
+    verify(mDependencyProvider).get(runnableProviderKey);
+
+    assertThat(testObject).isNotNull();
+    assertThat(mockingDetails(testObject).isMock()).isFalse();
+    assertThat(testObject.mRunnable).isEqualTo(mRunnableMock);
+    assertThat(testObject.mRunnableProvider).isEqualTo(mRunnableProviderMock);
+  }
+
+  @Test
+  public void testWithInjectOnly() {
+    prep(Inject.class);
+
+    when(mDependencyProvider.get(runnableKey)).thenReturn(mRunnableMock);
+    when(mDependencyProvider.get(runnableProviderKey)).thenReturn(mRunnableProviderMock);
+
+    TestClassWithInjectParams testObject = mRealObjectMaker.createObject(
+        mDependencyProvider,
+        TypeToken.of(TestClassWithInjectParams.class));
+
+    verify(mDependencyProvider).get(runnableKey);
+    verify(mDependencyProvider).get(runnableProviderKey);
+
+    assertThat(testObject).isNotNull();
+    assertThat(mockingDetails(testObject).isMock()).isFalse();
+    assertThat(testObject.mRunnable).isEqualTo(mRunnableMock);
+    assertThat(testObject.mRunnableProvider).isEqualTo(mRunnableProviderMock);
+  }
+
+  @Test
+  public void testWithConstructorAndInjectMixed() {
+    prep(Inject.class);
+
+    when(mDependencyProvider.get(runnableKey)).thenReturn(mRunnableMock);
+    when(mDependencyProvider.get(runnableProviderKey)).thenReturn(mRunnableProviderMock);
+
+    TestConstructorAndInject testObject = mRealObjectMaker.createObject(
+        mDependencyProvider,
+        TypeToken.of(TestConstructorAndInject.class));
+    
+    verify(mDependencyProvider).get(runnableKey);
+    verify(mDependencyProvider).get(runnableProviderKey);
+
+    assertThat(testObject).isNotNull();
+    assertThat(mockingDetails(testObject).isMock()).isFalse();
+    assertThat(testObject.mRunnable).isEqualTo(mRunnableMock);
+    assertThat(testObject.mRunnableProvider).isEqualTo(mRunnableProviderMock);
+  }
+
+  @SafeVarargs
+  private final void prep(Class<? extends Annotation>... annotations) {
+    List<Class<? extends Annotation>> annotationList = Arrays.asList(annotations);
+    mRealObjectMaker = new RealObjectMaker(mConstructorSelector, annotationList);
+  }
+
+  public static class TestClassWithConstructorOnly {
+    final Runnable mRunnable;
+    final Provider<Runnable> mRunnableProvider;
+
+    public TestClassWithConstructorOnly(
+        Runnable runnable,
+        @Named("testprovider") Provider<Runnable> runnableProvider) {
+      mRunnable = runnable;
+      mRunnableProvider = runnableProvider;
+    }
+  }
+
+  public static class TestClassWithInjectParams {
+    @Inject Runnable mRunnable;
+    @Inject @Named("testprovider") Provider<Runnable> mRunnableProvider;
+  }
+
+  public static class TestConstructorAndInject {
+    final Runnable mRunnable;
+    @Inject @Named("testprovider") Provider<Runnable> mRunnableProvider;
+
+    public TestConstructorAndInject(Runnable runnable) {
+      mRunnable = runnable;
+    }
+  }
+}
