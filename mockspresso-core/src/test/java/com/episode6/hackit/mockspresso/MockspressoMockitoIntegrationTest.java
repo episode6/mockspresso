ed8b1ec1249a41c45edf5eb888553dbb4e2ce9a7
Geoff Hackett <ghackett@gmail.com>
2017-03-12 23:25:23 -0400

    playing with the coffee maker example

--- mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/MockspressoMockitoIntegrationTest.java
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/MockspressoMockitoIntegrationTest.java
@@ -1,10 +1,16 @@
 package com.episode6.hackit.mockspresso;
 
 import com.episode6.hackit.chop.Chop;
+import com.episode6.hackit.mockspresso.annotation.RealObject;
 import com.episode6.hackit.mockspresso.mockito.MockspressoMockitoConfig;
+import com.episode6.hackit.mockspresso.testobject.CoffeeExample.*;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 /**
  * Integration test for Mockspresso usage with mockito
@@ -12,12 +18,20 @@ import org.junit.runner.RunWith;
 @RunWith(DefaultTestRunner.class)
 public class MockspressoMockitoIntegrationTest {
 
-  @Rule public final Mockspresso.Rule mockspresso = Mockspresso.Builders.simple()
+  @Rule public final Mockspresso.Rule mockspresso = Mockspresso.Builders.javaxInjection()
       .mockerConfig(new MockspressoMockitoConfig())
       .buildRule();
 
+  @Mock Heater mHeater;
+  @Mock Pump mPump;
+
+  @RealObject CoffeeMaker mCoffeeMaker;
+
   @Test
   public void placeholderTest() {
-    Chop.i("Mockito Rule Validated");
+    mCoffeeMaker.brew();
+
+    verify(mHeater).heat();
+    verify(mPump).pump();
   }
 }
