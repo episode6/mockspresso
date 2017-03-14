0ee8afd5463d12a5f5b4fbf9086e0aea9acee1c4
Geoff Hackett <ghackett@gmail.com>
2017-03-14 13:16:23 -0400

    update and refactor the first CoffeeMakers integration tests

---
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/integration/mockito/CoffeeMakersMockEverythingTestMockito.java
@@ -0,0 +1,151 @@
+package com.episode6.hackit.mockspresso.integration.mockito;
+
+import com.episode6.hackit.mockspresso.Mockspresso;
+import com.episode6.hackit.mockspresso.annotation.RealObject;
+import com.episode6.hackit.mockspresso.integration.testobjects.coffee.*;
+import com.episode6.hackit.mockspresso.integration.testobjects.coffee.CoffeeMakers.*;
+import com.episode6.hackit.mockspresso.mockito.MockitoPlugin;
+import com.episode6.hackit.mockspresso.plugin.javax.JavaxInjectMockspressoPlugin;
+import com.episode6.hackit.mockspresso.plugin.simple.SimpleInjectMockspressoPlugin;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+
+import static com.episode6.hackit.mockspresso.Conditions.mockitoMock;
+import static com.episode6.hackit.mockspresso.Conditions.rawClass;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Mockspresso integration test / example
+ * Tests {@link CoffeeMakers} with Mockito
+ *
+ * In this test, we mock all dependencies of the target classes we're testing.
+ *
+ * In the first test, we test the @RealObject that is created by the test's @Rule.
+ * In the rest of the tests we build upon the @Rule so we may create different types
+ * of DI-compatible objects
+ */
+@RunWith(JUnit4.class)
+public class CoffeeMakersMockEverythingTestMockito {
+
+  @Rule public final Mockspresso.Rule mockspresso = Mockspresso.Builders.simple()
+      .plugin(MockitoPlugin.getInstance())
+      .buildRule();
+
+  @Mock Water mWater;
+  @Mock Heater mHeater;
+  @Mock Pump mPump;
+  @Mock CoffeeGrounds mCoffeeGrounds;
+
+  @RealObject SimpleCoffeeMaker mCoffeeMaker;
+
+  @Before
+  public void setup() {
+    when(mPump.pump()).thenReturn(mWater);
+  }
+
+  /**
+   * Test a simple object with a normal constructor using, using the
+   * {@link SimpleInjectMockspressoPlugin}
+   */
+  @Test
+  public void testSimpleCoffeeMaker() {
+    assertThat(mCoffeeMaker).is(rawClass(SimpleCoffeeMaker.class));
+
+    Coffee coffee = mCoffeeMaker.brew();
+
+    verifyInterationsAndCoffeeObject(coffee);
+  }
+
+  /**
+   * Test an object that is created by javax.inject rules, using the
+   * {@link JavaxInjectMockspressoPlugin}
+   */
+  @Test
+  public void testConstructorInjectedCoffeeMaker() {
+    ConstructorInjectedCofferMaker cofferMaker = injectionMockspresso()
+        .create(ConstructorInjectedCofferMaker.class);
+    assertThat(cofferMaker).is(rawClass(ConstructorInjectedCofferMaker.class));
+
+    Coffee coffee = cofferMaker.brew();
+
+    verifyInterationsAndCoffeeObject(coffee);
+  }
+
+  /**
+   * Test an object that is created by javax.inject rules, using the
+   * {@link JavaxInjectMockspressoPlugin}
+   */
+  @Test
+  public void testFieldInjedCoffeeMaker() {
+    FieldInjectedCoffeeMakerWithGroundsProvider cofferMaker = injectionMockspresso()
+        .create(FieldInjectedCoffeeMakerWithGroundsProvider.class);
+    assertThat(cofferMaker).is(rawClass(FieldInjectedCoffeeMakerWithGroundsProvider.class));
+
+    Coffee coffee = cofferMaker.brew();
+
+    verifyInterationsAndCoffeeObject(coffee);
+  }
+
+  /**
+   * Test an object that is created by javax.inject rules, using the
+   * {@link JavaxInjectMockspressoPlugin}
+   */
+  @Test
+  public void testMethodInjectedCoffeeMaker() {
+    MethodInjectedCoffeeMaker cofferMaker = injectionMockspresso()
+        .create(MethodInjectedCoffeeMaker.class);
+    assertThat(cofferMaker).is(rawClass(MethodInjectedCoffeeMaker.class));
+
+    Coffee coffee = cofferMaker.brew();
+
+    verifyInterationsAndCoffeeObject(coffee);
+  }
+
+  /**
+   * Test an object that is created by javax.inject rules, using the
+   * {@link JavaxInjectMockspressoPlugin}
+   */
+  @Test
+  public void testMixedInjectionCoffeeMaker() {
+    MixedInjectionCoffeeMaker cofferMaker = injectionMockspresso()
+        .create(MixedInjectionCoffeeMaker.class);
+    assertThat(cofferMaker).is(rawClass(MixedInjectionCoffeeMaker.class));
+
+    Coffee coffee = cofferMaker.brew();
+
+    verifyInterationsAndCoffeeObject(coffee);
+  }
+
+  /**
+   * @return a new mockspresso instance with the {@link JavaxInjectMockspressoPlugin} applied.
+   */
+  private Mockspresso injectionMockspresso() {
+    return mockspresso.buildUpon()
+        .plugin(JavaxInjectMockspressoPlugin.getInstance())
+        .build();
+  }
+
+  private void verifyInterationsAndCoffeeObject(Coffee coffee) {
+    // verify our defined pump and heater mocks were called
+    verify(mPump).pump();
+    verify(mHeater).heat(mWater);
+
+    // assert that coffee was actually created contains the Water
+    // and CoffeeGrounds objects we expect
+    assertThat(coffee)
+        .isNotNull()
+        .isNot(mockitoMock());
+    assertThat(coffee.getWater())
+        .isNotNull()
+        .isEqualTo(mWater);
+    assertThat(coffee.getCoffeeGrounds())
+        .isNotNull()
+        .isEqualTo(mCoffeeGrounds);
+  }
+}
--- mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/integration/testobjects/coffee/CoffeeMakers.java
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/integration/testobjects/coffee/CoffeeMakers.java
@@ -1,10 +1,15 @@
 package com.episode6.hackit.mockspresso.integration.testobjects.coffee;
 
+import com.episode6.hackit.mockspresso.plugin.javax.JavaxInjectMockspressoPlugin;
+import com.episode6.hackit.mockspresso.plugin.javax.JavaxInjectionConfig;
+import com.episode6.hackit.mockspresso.plugin.simple.SimpleInjectMockspressoPlugin;
+
 import javax.inject.Inject;
 import javax.inject.Provider;
 
 /**
- * Different implementations of a coffee maker to use in integration tests
+ * Different implementations of a coffee maker to use in integration tests. Every coffee maker
+ * uses the same implementation of brew() so we can keep these tests simple.
  */
 public class CoffeeMakers {
 
@@ -18,7 +23,7 @@ public class CoffeeMakers {
   }
 
   /**
-   * The most basic coffee maker, can be created via the simple inject plugin
+   * The most basic coffee maker, can be created via the {@link SimpleInjectMockspressoPlugin}
    */
   public static class SimpleCoffeeMaker implements CoffeeMaker {
     private final Pump mPump;
@@ -37,10 +42,84 @@ public class CoffeeMakers {
     }
   }
 
+  /**
+   * A coffee make that is created via constructor injection
+   */
+  public static class ConstructorInjectedCofferMaker implements CoffeeMaker {
+    private final Pump mPump;
+    private final Heater mHeater;
+    private final CoffeeGrounds mCoffeeGrounds;
+
+    @Inject
+    public ConstructorInjectedCofferMaker(Pump pump, Heater heater, CoffeeGrounds coffeeGrounds) {
+      mPump = pump;
+      mHeater = heater;
+      mCoffeeGrounds = coffeeGrounds;
+    }
+
+    @Override
+    public Coffee brew() {
+      return brewInternal(mPump, mHeater, mCoffeeGrounds);
+    }
+  }
+
+  /**
+   * A coffee maker that is created via Field injection. It also expects
+   * a {@link Provider<CoffeeGrounds>} instead of a {@link CoffeeGrounds} object.
+   * This provider can be handled automatically via the {@link JavaxInjectMockspressoPlugin}.
+   */
   public static class FieldInjectedCoffeeMakerWithGroundsProvider implements CoffeeMaker {
     @Inject Pump mPump;
     @Inject Heater mHeater;
     @Inject Provider<CoffeeGrounds> mCoffeeGroundsProvider;
+
+    @Override
+    public Coffee brew() {
+      return brewInternal(mPump, mHeater, mCoffeeGroundsProvider.get());
+    }
+  }
+
+  /**
+   * A coffee maker that is created via method injection.
+   */
+  public static class MethodInjectedCoffeeMaker implements CoffeeMaker {
+    private Pump mPump;
+    private Heater mHeater;
+    private CoffeeGrounds mCoffeeGrounds;
+
+    @Inject
+    public final void injectMethodInjectedCoffeeMaker(Pump pump, Heater heater, CoffeeGrounds coffeeGrounds) {
+      mPump = pump;
+      mHeater = heater;
+      mCoffeeGrounds = coffeeGrounds;
+    }
+
+    @Override
+    public Coffee brew() {
+      return brewInternal(mPump, mHeater, mCoffeeGrounds);
+    }
+  }
+
+  /**
+   * A coffee make that is created via constructor injection, method injection
+   * and field injection.
+   *
+   * This probably wouldn't be the smartest way to write a class irl.
+   */
+  public static class MixedInjectionCoffeeMaker implements CoffeeMaker {
+    private final Pump mPump;
+    private Heater mHeater;
+    @Inject Provider<CoffeeGrounds> mCoffeeGroundsProvider;
+
+    @Inject
+    public MixedInjectionCoffeeMaker(Pump pump) {
+      mPump = pump;
+    }
+
+    @Inject
+    public final void injectMixedInjectionCoffeeMaker(Heater heater) {
+      mHeater = heater;
+    }
 
     @Override
     public Coffee brew() {
