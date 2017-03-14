7f681d30a446f14f03cdaa7bba5b70d11b151a00
Geoff Hackett <ghackett@gmail.com>
2017-03-14 11:11:56 -0400

    refactor the two first sample integration tests

--- mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/integration/mockito/SimpleCoffeeMakerMockitoTest.java
+++ mockspresso-core/src/test/java/com/episode6/hackit/mockspresso/integration/mockito/SimpleCoffeeMakerMockitoTest.java
@@ -1,6 +1,5 @@
 package com.episode6.hackit.mockspresso.integration.mockito;
 
-import com.episode6.hackit.mockspresso.DefaultTestRunner;
 import com.episode6.hackit.mockspresso.Mockspresso;
 import com.episode6.hackit.mockspresso.annotation.RealObject;
 import com.episode6.hackit.mockspresso.integration.testobjects.coffee.*;
@@ -8,6 +7,7 @@ import com.episode6.hackit.mockspresso.mockito.MockitoPlugin;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 
 import static com.episode6.hackit.mockspresso.Conditions.mockitoMock;
@@ -16,10 +16,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
- * Integration test for Mockspresso usage with mockito
+ * Mockspresso integration test / example
+ * Tests {@link CoffeeMakers.SimpleCoffeeMaker} with Mockito
  */
-@RunWith(DefaultTestRunner.class)
-public class MockspressoMockitoIntegrationTest {
+@RunWith(JUnit4.class)
+public class SimpleCoffeeMakerMockitoTest {
 
   @Rule public final Mockspresso.Rule mockspresso = Mockspresso.Builders.simple()
       .plugin(MockitoPlugin.getInstance())
@@ -32,7 +33,7 @@ public class MockspressoMockitoIntegrationTest {
   @RealObject CoffeeMakers.SimpleCoffeeMaker mCoffeeMaker;
 
   @Test
-  public void placeholderTest() {
+  public void testBrew() {
     when(mPump.pump()).thenReturn(mWater);
 
     Coffee coffee = mCoffeeMaker.brew();
