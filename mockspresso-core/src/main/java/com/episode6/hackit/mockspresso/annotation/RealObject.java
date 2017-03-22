9756a1cb3a89561450cb55d9b329735d695e066d
Geoff Hackett <ghackett@gmail.com>
2017-03-22 12:44:40 -0400

    restrict @RealObject so it may only be applied to Fields

--- mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/annotation/RealObject.java
+++ mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/annotation/RealObject.java
@@ -1,13 +1,12 @@
 package com.episode6.hackit.mockspresso.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
 /**
  * Annotation that represents a real object used in a test (as opposed to a mock)
  */
 @Documented
+@Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RealObject {
 
