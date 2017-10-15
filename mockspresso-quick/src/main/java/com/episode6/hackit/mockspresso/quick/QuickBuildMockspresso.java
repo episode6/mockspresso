3545c6c8fe06e603a6459028ab0c22ef7d3d079d
Geoff Hackett <ghackett@gmail.com>
2017-10-15 11:39:34 -0400

    need a way to buildUpon using the quick mockspresso builder

--- mockspresso-quick/src/main/java/com/episode6/hackit/mockspresso/quick/QuickBuildMockspresso.java
+++ mockspresso-quick/src/main/java/com/episode6/hackit/mockspresso/quick/QuickBuildMockspresso.java
@@ -1,6 +1,7 @@
 package com.episode6.hackit.mockspresso.quick;
 
 import com.episode6.hackit.mockspresso.BuildMockspresso;
+import com.episode6.hackit.mockspresso.Mockspresso;
 import com.episode6.hackit.mockspresso.quick.internal.QuickMockspressoBuilderImpl;
 
 /**
@@ -10,5 +11,9 @@ public class QuickBuildMockspresso {
 
   public static QuickMockspressoBuilder with() {
     return new QuickMockspressoBuilderImpl(BuildMockspresso.with());
+  }
+
+  public static QuickMockspressoBuilder buildUpon(Mockspresso mockspresso) {
+    return new QuickMockspressoBuilderImpl(mockspresso.buildUpon());
   }
 }
