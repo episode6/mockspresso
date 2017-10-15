280deaddb5b0eee879d4923e49ebb9b7dccca2f2
Geoff Hackett <ghackett@gmail.com>
2017-10-15 13:13:45 -0400

    update mockspresso-mockito tests

--- mockspresso-mockito/src/test/java/com/episode6/hackit/mockspresso/mockito/integration/AutoFactoryTest.java
+++ mockspresso-mockito/src/test/java/com/episode6/hackit/mockspresso/mockito/integration/AutoFactoryTest.java
@@ -1,8 +1,8 @@
 package com.episode6.hackit.mockspresso.mockito.integration;
 
-import com.episode6.hackit.mockspresso.BuildMockspresso;
 import com.episode6.hackit.mockspresso.Mockspresso;
 import com.episode6.hackit.mockspresso.annotation.RealObject;
+import com.episode6.hackit.mockspresso.quick.QuickBuildMockspresso;
 import com.episode6.hackit.mockspresso.testing.testobjects.coffee.Coffee;
 import com.episode6.hackit.mockspresso.testing.testobjects.coffee.CoffeeGrounds;
 import com.episode6.hackit.mockspresso.testing.testobjects.coffee.CoffeeGroundsFactory;
@@ -22,7 +22,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
 @RunWith(JUnit4.class)
 public class AutoFactoryTest {
 
-  @Rule public final Mockspresso.Rule mockspresso = BuildMockspresso.with()
+  @Rule public final Mockspresso.Rule mockspresso = QuickBuildMockspresso.with()
       .injector().simple()
       .mocker().mockito()
       .plugin().automaticFactories(CoffeeGroundsFactory.class)
