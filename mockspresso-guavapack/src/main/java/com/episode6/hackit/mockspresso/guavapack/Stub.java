4e6502865b2b5797e3f51e9bbab63b9ffded0471
Geoff <ghackett@gmail.com>
2019-03-12 18:19:07 -0400

    now that we're using a normal library, tests should start passing

--- mockspresso-guavapack/src/main/java/com/episode6/hackit/mockspresso/guavapack/Stub.java
+++
@@ -1,9 +0,0 @@
-package com.episode6.hackit.mockspresso.guavapack;
-
-import com.google.common.reflect.TypeToken;
-
-class Stub {
-    private static TypeToken<String> get() {
-        return new TypeToken<String>() {};
-    }
-}
