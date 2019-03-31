59233c30430689ed094c487d306f47537ae95bfa
Geoff <ghackett@gmail.com>
2019-03-31 11:04:56 -0400

    clean things up a bit

--- mockspresso-api/src/main/java/com/episode6/hackit/mockspresso/MockspressoExt.kt
+++ mockspresso-api/src/main/java/com/episode6/hackit/mockspresso/MockspressoExt.kt
@@ -7,5 +7,14 @@ import com.episode6.hackit.mockspresso.reflect.dependencyKey
  * Kotlin extensions to mockspresso's api
  */
 
-inline fun <reified T : Any> Mockspresso.Builder.dependencyOf(qualifier: Annotation? = null, noinline valueProvider: ()->T?): Mockspresso.Builder =
-    dependencyProvider(dependencyKey<T>(qualifier), ObjectProvider<T>(valueProvider))
+/**
+ * Apply a specific instance of an object as a mockspresso dependency.
+ * Kotlin alias for [Mockspresso.Builder.dependencyProvider].
+ *
+ * @param qualifier Optional qualifier annotation that applies to the binding key of this dependency
+ * @param value Kotlin function/lambda that returns the dependency
+ */
+inline fun <reified T : Any> Mockspresso.Builder.dependencyOf(
+    qualifier: Annotation? = null,
+    noinline value: ()->T?
+): Mockspresso.Builder = dependencyProvider(dependencyKey<T>(qualifier), ObjectProvider<T>(value))
