aabd07694df6beb618f760275cf067c05403a278
Geoff <ghackett@gmail.com>
2019-04-02 18:40:35 -0400

    [Kotlin] Add kotlin extensions for typed Mockspresso api methods

--- mockspresso-api/src/main/java/com/episode6/hackit/mockspresso/MockspressoExt.kt
+++ mockspresso-api/src/main/java/com/episode6/hackit/mockspresso/MockspressoExt.kt
@@ -42,3 +42,33 @@ inline fun <reified BIND : Any, reified IMPL : BIND> Mockspresso.Builder.realImp
 inline fun <reified BIND_AND_IMPL : Any> Mockspresso.Builder.realClassOf(
     qualifier: Annotation? = null
 ): Mockspresso.Builder = realImplOf<BIND_AND_IMPL, BIND_AND_IMPL>(qualifier)
+
+/**
+ * Create a real object of type [T] injected with mockspresso dependencies.
+ *
+ * @return An instance of [T]
+ */
+inline fun <reified T : Any> Mockspresso.createNew(): T = create(typeToken<T>())
+
+/**
+ * Inject an existing object with mockspresso dependencies.
+ * Field and method injection will be performed (assuming the
+ * injector of this mockspresso instance supports it). Uses a reified
+ * type and includes a TypeToken in the method call, so will work with
+ * generics.
+ *
+ * @param instance The object to inject mocks/dependencies into.
+ */
+inline fun <reified T : Any> Mockspresso.injectType(
+    instance: T
+): Unit = inject(instance, typeToken<T>())
+
+/**
+ * Get a dependency (creating a new mock, if needed) from mockspresso.
+ * 
+ * @param qualifier The optional qualifier annotation for this object's
+ * [com.episode6.hackit.mockspresso.reflect.DependencyKey]
+ */
+inline fun <reified T : Any> Mockspresso.getDependencyOf(
+    qualifier: Annotation? = null
+): T? = getDependency(dependencyKey<T>(qualifier))
