7da40680d3cccf797d1d0436ae08014348f0a3b6
Geoff <ghackett@gmail.com>
2019-04-07 17:22:21 -0400

    Fix dokka crash

--- mockspresso-quick/src/main/java/com/episode6/hackit/mockspresso/quick/BuildQuickMockspresso.java
+++ mockspresso-quick/src/main/java/com/episode6/hackit/mockspresso/quick/BuildQuickMockspresso.java
@@ -6,14 +6,14 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * Entry-point to create new instances of {@link QuickMockspresso.Builder}
- * @deprecated see {@link DEPRECATEDKt}
+ * @deprecated see DEPRECATED.kt
  */
 @Deprecated
 public class BuildQuickMockspresso {
 
   /**
    * @return a brand new instance of {@link QuickMockspresso.Builder}
-   * @deprecated see {@link DEPRECATEDKt}
+   * @deprecated see DEPRECATED.kt
    */
   @Deprecated
   @NotNull public static QuickMockspresso.Builder with() {
@@ -24,7 +24,7 @@ public class BuildQuickMockspresso {
    * Builds upon an existing mockspresso instance with a new {@link QuickMockspresso.Builder}
    * @param mockspresso The {@link Mockspresso} instance to wrap
    * @return and new {@link QuickMockspresso.Builder} built upon the supplied mockspresso instance.
-   * @deprecated see {@link DEPRECATEDKt}
+   * @deprecated see DEPRECATED.kt
    */
   @Deprecated
   @NotNull public static QuickMockspresso.Builder upon(@NotNull Mockspresso mockspresso) {
