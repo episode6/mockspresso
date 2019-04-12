ba3b4544b4ae9dc8331e1178c7c1817cbffb6d64
Geoff <ghackett@gmail.com>
2019-04-12 07:26:18 -0400

    Convert daggerPluginsTest to mockk (will be needed to test that external extension methods are called)

--- mockspresso-dagger/src/test/java/com/episode6/hackit/mockspresso/dagger/DaggerPluginsExtTest.kt
+++ mockspresso-dagger/src/test/java/com/episode6/hackit/mockspresso/dagger/DaggerPluginsExtTest.kt
@@ -1,32 +1,44 @@
 package com.episode6.hackit.mockspresso.dagger
 
 import com.episode6.hackit.mockspresso.Mockspresso
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
+import com.episode6.hackit.mockspresso.api.MockspressoPlugin
+import com.episode6.hackit.mockspresso.api.SpecialObjectMaker
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
 import org.fest.assertions.api.Assertions.assertThat
 import org.junit.Test
-import org.mockito.stubbing.Answer
+
 
 /**
  * Tests [com.episode6.hackit.mockspresso.dagger.DaggerPluginsExtKt]
  */
 class DaggerPluginsExtTest {
 
-  val builder: Mockspresso.Builder = mock(defaultAnswer = Answer { it.mock })
+  val builder = mockk<Mockspresso.Builder>().apply {
+    every { plugin(any()) } returns this
+    every { specialObjectMaker(any()) } returns this
+  }
 
   @Test
   fun testSimplePluginSourceOfTruth() {
+    val slot = slot<MockspressoPlugin>()
+
     val result = builder.injectByDaggerConfig()
 
     assertThat(result).isEqualTo(builder)
-    verify(builder).plugin(any<DaggerMockspressoPlugin>())
+    verify { builder.plugin(capture(slot)) }
+    assertThat(slot.captured).isInstanceOf(DaggerMockspressoPlugin::class.java)
   }
 
   @Test fun testAutomaticLaziesSourceOfTruth() {
+    val slot = slot<SpecialObjectMaker>()
+
     val result = builder.automaticLazies()
 
     assertThat(result).isEqualTo(builder)
-    verify(builder).specialObjectMaker(any<DaggerLazyMaker>())
+    verify { builder.specialObjectMaker(capture(slot)) }
+    assertThat(slot.captured).isInstanceOf(DaggerLazyMaker::class.java)
   }
 }
