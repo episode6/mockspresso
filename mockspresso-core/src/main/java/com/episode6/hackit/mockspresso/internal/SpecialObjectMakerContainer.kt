1b09321e29f7a6b11d7add48b1e122a821ca2dea
Geoff Hackett <ghackett@gmail.com>
2020-09-26 10:20:01 -0400

    [CONVERT] The last SpecialObjectMaker to kotlin

---
+++ mockspresso-core/src/main/java/com/episode6/hackit/mockspresso/internal/SpecialObjectMakerContainer.kt
@@ -0,0 +1,42 @@
+package com.episode6.hackit.mockspresso.internal
+
+import com.episode6.hackit.mockspresso.api.DependencyProvider
+import com.episode6.hackit.mockspresso.api.SpecialObjectMaker
+import com.episode6.hackit.mockspresso.reflect.DependencyKey
+import java.util.*
+
+/**
+ * A container for special object makers. Includes a parent maker (usually
+ * another instance of SpecialObjectMakerContainer) that is checked last.
+ */
+internal class SpecialObjectMakerContainer : SpecialObjectMaker {
+  private var parent: SpecialObjectMaker? = null
+  private val makers: MutableList<SpecialObjectMaker> = LinkedList()
+
+  fun deepCopy(): SpecialObjectMakerContainer = SpecialObjectMakerContainer().also {
+    it.setParentMaker(parent)
+    it.addAll(makers)
+  }
+
+  fun setParentMaker(parentMaker: SpecialObjectMaker?) {
+    parent = parentMaker
+  }
+
+  fun add(specialObjectMaker: SpecialObjectMaker) {
+    makers.add(specialObjectMaker)
+  }
+
+  fun addAll(specialObjectMakers: Collection<SpecialObjectMaker>) {
+    makers.addAll(specialObjectMakers)
+  }
+
+  override fun canMakeObject(key: DependencyKey<*>): Boolean = when {
+    findMakerFor(key) != null -> true
+    else                      -> parent?.canMakeObject(key) ?: false
+  }
+
+  override fun <T> makeObject(dependencyProvider: DependencyProvider, key: DependencyKey<T>): T? =
+      (findMakerFor(key) ?: parent)?.makeObject(dependencyProvider, key)
+
+  private fun findMakerFor(key: DependencyKey<*>) = (makers.firstOrNull { it.canMakeObject(key) })
+}
