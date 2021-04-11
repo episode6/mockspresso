4ba08100eace303cd688cccde3e3f83411554b8e
Geoff Hackett <ghackett@gmail.com>
2021-04-11 11:08:51 -0400

    get dokka working (was forced to upgrade)

--- buildSrc/src/main/groovy/DeployableJavaProject.groovy
+++ buildSrc/src/main/groovy/DeployableJavaProject.groovy
@@ -1,8 +1,6 @@
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.plugins.PluginContainer
-import org.gradle.api.tasks.Sync
 import org.gradle.api.tasks.bundling.Jar
 
 class DeployableJavaProject extends JavaProject {
@@ -29,9 +27,9 @@ class DeployableJavaProject extends JavaProject {
         }
       }
 
-      task("javadocJar", type: Jar, dependsOn: tasks.dokka) {
+      task("javadocJar", type: Jar, dependsOn: tasks.dokkaHtml) {
         archiveClassifier = 'javadoc'
-        from tasks.dokka
+        from tasks.dokkaHtml
       }
 
       task("sourcesJar", type: Jar) {
@@ -39,39 +37,20 @@ class DeployableJavaProject extends JavaProject {
         archiveClassifier = 'sources'
       }
 
-      task(Tasks.SYNC_DOCS, type: Sync) {
-        from tasks.dokka.outputDirectory
-        into "${rootProject.rootDir}/docs/javadocs/${name}"
-        dependsOn tasks.dokka
-
-        // this file doesn't get generated correctly in the current version of dokka
-        // each link in it has two nested <a href> tags, where the inner-one is correct
-        // but the outer one references the absolute path where the docs were first
-        // generated (i.e. my laptop).
-        // This file also isn't linked anywhere from the rest of the javadocs
-        // so excluding it for now should be NBD.
-        excludes += "${name}/index-outline.html"
-      }
-
-      tasks.dokka {
-        jdkVersion = 8
+      tasks.dokkaHtml {
+        dokkaSourceSets {
+          configureEach {
+            jdkVersion.set(8)
+            reportUndocumented.set(false)
+            noStdlibLink.set(false)
+            noJdkLink.set(false)
+          }
+        }
       }
 
       configurations.all { Configuration config ->
         //noinspection UnstableApiUsage
         config.withDependencies { dependencies ->
-
-          // Each module's javadocs are generated separately and can't see each other (atm).
-          // This block links to our already hosted docs from the previous release.
-          // It's not ideal, but its better than nothing.
-          dependencies.findAll { it instanceof ProjectDependency }
-              .forEach { dep ->
-                project.tasks.dokka {
-                  externalDocumentationLink {
-                    url = new URL("https://episode6.github.io/mockspresso/javadocs/${dep.name}/${dep.name}/")
-                  }
-                }
-              }
 
 //          // Include links to javadocs of external dependencies when possible
 //          dependencies.findAll { it instanceof ExternalDependency }
